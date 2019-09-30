82c9204606a2a4fbe8a5c6578a58c460f95f18a5
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-09-30 21:51:22 +0200

    [SERVICES] Migrate to a cli-injector

---
+++ services/devops/injector/injector.js
@@ -0,0 +1,36 @@
+// Init script
+const path = require('path');
+const fs = require('fs');
+const program = require('commander');
+const chalk = require('chalk');
+
+if (!fs.existsSync(path.resolve(__dirname, '../../dist'))) {
+    console.error(error);
+    process.exit(1);
+}
+
+program
+    .option('-v, --verbose', 'Log every injection done')
+    .option('-q, --quantity <number>', 'Quantity to inject', 100)
+    .option('--businesses', 'Inject businesses')
+    .parse(process.argv);
+
+const dotenv = require('dotenv');
+dotenv.config({ path: path.resolve(__dirname, '../../dist/.env') });
+
+require('../../dist/configs/instances/database'); // Only import to setup
+require('../../dist/configs/setup/database'); // Only import to setup
+
+const BusinessesLoader = require('./fake-load/businesses');
+
+Promise.resolve().then(async () => {
+    try {
+        if (program.businesses) await BusinessesLoader(program.quantity, program.verbose);
+
+        console.info(chalk.blue('Successfully setup data.'));
+        process.exit(0);
+    } catch (error) {
+        console.error(error);
+        process.exit(1);
+    }
+});
