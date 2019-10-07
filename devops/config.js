f854add72ace9a504f683265d55ddff73c8f820a
Oscar MARIE--TAILLEFER <o5mariet@enib.fr>
2019-10-07 20:26:28 +0200

    [DEVOPS] Devops directory with deployment utils
    Add devops directory with
    
    * A gitignore file for production
    * A config file to configure deployment script
    * a deployment script to deploy to github repository production build
    * Remove previous deploy.sh script

---
+++ devops/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Internships manager',
+    tagline: 'Application to manage ENIB interships system',
+    url: 'https://ryshu.github.io',
+    baseUrl: '/enib-internships/',
+
+    // Used for publishing and more
+    deployBranch: 'production',
+    projectName: 'enib-internships',
+    organizationName: 'enib',
+};
