c3be6adeaadcb5ab52e56e6b43943e0b0b62ce8b
Andre Felix <alfal_35547@hotmail.com>
2019-10-09 14:41:20 +0200

    [SERVICES] Add internship to route
    Modify route

--- services/src/api/route.ts
+++ services/src/api/route.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import businessesRouter from './routers/businesses.route';
+import internshipsRouter from './routers/internships.route';
 import mockUserSystem from '../mock/route';
 
 const router = express.Router();
 
 router.use('/businesses', businessesRouter);
+router.use('/internships', internshipsRouter);
 router.use(mockUserSystem);
 
 export default router;
