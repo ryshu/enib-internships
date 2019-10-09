c837a4b6437b1463db599c4e35b945943a99b1f4
Andre Felix <alfal_35547@hotmail.com>
2019-10-09 14:34:00 +0200

    [SERVICES] Setup internships
    Create
    Modify
    Delete

---
+++ services/src/api/controllers/internships.ctrl.ts
@@ -0,0 +1,161 @@
+import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator';
+
+import Internships from '../../models/Internships';
+import httpStatus from 'http-status-codes';
+
+import { paginate } from '../helpers/pagination.helper';
+import {
+    UNPROCESSABLE_ENTITY,
+    checkArrayContent,
+    BAD_REQUEST_VALIDATOR,
+    checkContent,
+} from '../helpers/global.helper';
+
+/**
+ * GET /internships
+ * Used to GET all internships
+ */
+export const getInternships = (req: Request, res: Response, next: NextFunction): void => {
+    // @see validator + router
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return BAD_REQUEST_VALIDATOR(next, errors);
+    }
+
+    // Retrive query data
+    const { page = 1, limit = 20 } = req.query;
+    let max: number;
+    Internships.count()
+        .then((rowNbr) => {
+            max = rowNbr;
+            return Internships.findAll(paginate({ page, limit }));
+        })
+        .then((internships) => {
+            if (checkArrayContent(internships, next)) {
+                return res.send({
+                    page,
+                    data: internships,
+                    length: internships.length,
+                    max,
+                });
+            }
+        })
+        .catch((e) => UNPROCESSABLE_ENTITY(next, e));
+};
+
+/**
+ * POST /internship
+ * Used to create a new internship entry
+ */
+export const postInternship = (req: Request, res: Response, next: NextFunction): void => {
+    // @see validator + router
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return BAD_REQUEST_VALIDATOR(next, errors);
+    }
+
+    const internship: IInternshipEntity = {
+        subject: req.body.subject,
+        description: req.body.description,
+        country: req.body.country,
+        city: req.body.city,
+        address: req.body.address,
+        additional: req.body.additional,
+        isLanguageCourse: req.body.isLanguageCourse,
+        isValidated: req.body.isValidated,
+    };
+
+    Internships.create(internship)
+        .then((created) => res.send(created))
+        .catch((e) => UNPROCESSABLE_ENTITY(next, e));
+};
+
+/**
+ * GET /internship/:id
+ * Used to select a internship by ID
+ */
+export const getInternship = (req: Request, res: Response, next: NextFunction): void => {
+    // @see validator + router
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return BAD_REQUEST_VALIDATOR(next, errors);
+    }
+
+    Internships.findByPk(req.params.id)
+        .then((val) => {
+            if (checkContent(val, next)) {
+                return res.send(val);
+            }
+        })
+        .catch((e) => UNPROCESSABLE_ENTITY(next, e));
+};
+
+/**
+ * PUT /internship/:id
+ * Used to update internship values
+ */
+export const putInternship = (req: Request, res: Response, next: NextFunction): void => {
+    // @see validator + router
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return BAD_REQUEST_VALIDATOR(next, errors);
+    }
+
+    Internships.findByPk(req.params.id)
+        .then((internships) => {
+            if (!checkContent(internships, next)) {
+                return undefined;
+            }
+
+            if (req.body.subject) {
+                internships.set('subject', req.body.subject);
+            }
+            if (req.body.description) {
+                internships.set('description', req.body.description);
+            }
+            if (req.body.country) {
+                internships.set('country', req.body.country);
+            }
+            if (req.body.city) {
+                internships.set('city', req.body.city);
+            }
+            if (req.body.address) {
+                internships.set('address', req.body.address);
+            }
+            if (req.body.additional) {
+                internships.set('additional', req.body.additional);
+            }
+            if (req.body.isLanguageCourse) {
+                internships.set('isLanguageCourse', req.body.isLanguageCourse);
+            }
+            if (req.body.isValidated) {
+              internships.set('isValidated', req.body.isValidated);
+             }
+
+            return internships.save();
+        })
+        .then((updated) => {
+            if (updated) {
+                return res.send(updated);
+            }
+        })
+        .catch((e) => UNPROCESSABLE_ENTITY(e, next));
+};
+
+/**
+ * DELETE /internship/:id
+ * Used to remove a internship from database
+ */
+export const deleteInternship = (req: Request, res: Response, next: NextFunction): void => {
+    // @see validator + router
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return BAD_REQUEST_VALIDATOR(next, errors);
+    }
+
+    Internships.findByPk(req.params.id)
+        .then((val) => (val ? val.destroy() : undefined))
+        .then(() => res.sendStatus(httpStatus.OK))
+        .catch((e) => UNPROCESSABLE_ENTITY(e, next));
+};
---
+++ services/src/api/validators/internships.val.ts
@@ -0,0 +1,137 @@
+import { Schema } from 'express-validator';
+
+export const InternshipsList: Schema = {
+    page: {
+        in: ['query'],
+        isInt: { errorMessage: 'Page number must be an integer' },
+        optional: true,
+        toInt: true,
+    },
+    limit: {
+        in: ['query'],
+        isInt: { errorMessage: 'Limit of entries to provide must be an integer' },
+        optional: true,
+        toInt: true,
+    },
+};
+
+export const InternshipCreate: Schema = {
+    subject: {
+        in: ['body'],
+        isString: { errorMessage: 'Subject must be of type string' },
+        exists: { errorMessage: 'Subject must be defined' },
+        trim: true,
+        escape: true,
+    },
+    description: {
+        in: ['body'],
+        isString: { errorMessage: 'Description must be of type string' },
+        exists: { errorMessage: 'Description must be defined' },
+        trim: true,
+        escape: true,
+    },
+    country: {
+        in: ['body'],
+        isString: { errorMessage: 'Country must be of type string' },
+        exists: { errorMessage: 'Country must be defined' },
+        trim: true,
+        escape: true,
+    },
+    city: {
+        in: ['body'],
+        isString: { errorMessage: 'City must be of type string' },
+        exists: { errorMessage: 'City must be defined' },
+        trim: true,
+        escape: true,
+    },
+    address: {
+        in: ['body'],
+        isString: { errorMessage: 'Address must be of type string' },
+        exists: { errorMessage: 'Address must be defined' },
+        trim: true,
+        escape: true,
+    },
+    additional: {
+        in: ['body'],
+        isString: { errorMessage: 'Additional must be of type string' },
+        optional: true,
+        trim: true,
+        escape: true,
+    },
+    isLanguageCourse: {
+        in: ['body'],
+        isBoolean: { errorMessage: 'LanguageCourse must be of type boolean' },
+        exists: { errorMessage: 'LanguageCourse must be defined' },
+        escape: true,
+    },
+    isValidated: {
+        in: ['body'],
+        isBoolean: { errorMessage: 'Validated must be of type boolean' },
+        exists: { errorMessage: 'Validated must be defined' },
+        escape: true,
+    },
+};
+
+export const InternshipUpdate: Schema = {
+    subject: {
+      in: ['body'],
+      isString: { errorMessage: 'Subject must be of type string' },
+      exists: { errorMessage: 'Subject must be defined' },
+      trim: true,
+      escape: true,
+    },
+    description: {
+        in: ['body'],
+        isString: { errorMessage: 'Description must be of type string' },
+        exists: { errorMessage: 'Description must be defined' },
+        trim: true,
+        escape: true,
+    },
+    country: {
+        in: ['body'],
+        isString: { errorMessage: 'Country must be of type string' },
+        optional: true,
+        trim: true,
+        escape: true,
+    },
+    city: {
+        in: ['body'],
+        isString: { errorMessage: 'City must be of type string' },
+        optional: true,
+        trim: true,
+        escape: true,
+    },
+    postalCode: {
+        in: ['body'],
+        isString: { errorMessage: 'Postal Code must be of type string' },
+        optional: true,
+        trim: true,
+        escape: true,
+    },
+    address: {
+        in: ['body'],
+        isString: { errorMessage: 'Address must be of type string' },
+        optional: true,
+        trim: true,
+        escape: true,
+    },
+    additional: {
+        in: ['body'],
+        isString: { errorMessage: 'Additional must be of type string' },
+        optional: true,
+        trim: true,
+        escape: true,
+    },
+    isLanguageCourse: {
+      in: ['body'],
+      isBoolean: { errorMessage: 'LanguageCourse must be of type boolean' },
+      exists: { errorMessage: 'LanguageCourse must be defined' },
+      escape: true,
+    },
+    isValidated: {
+        in: ['body'],
+        isBoolean: { errorMessage: 'Validated must be of type boolean' },
+        exists: { errorMessage: 'Validated must be defined' },
+        escape: true,
+    },
+};
