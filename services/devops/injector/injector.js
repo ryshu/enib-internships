97e0ea31e9bf8f9a4456c8675d8f3c8eadc11c9d
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-10-10 19:26:47 +0200

    [INJECTOR] Add injector for internships and students

--- services/devops/injector/injector.js
+++ services/devops/injector/injector.js
@@ -13,6 +13,8 @@ program
     .option('-v, --verbose', 'Log every injection done')
     .option('-q, --quantity <number>', 'Quantity to inject', 100)
     .option('--businesses', 'Inject businesses')
+    .option('--internships', 'Inject interships')
+    .option('--students', 'Inject students')
     .parse(process.argv);
 
 const dotenv = require('dotenv');
@@ -22,10 +24,14 @@ require('../../dist/configs/instances/database'); // Only import to setup
 require('../../dist/configs/setup/database'); // Only import to setup
 
 const BusinessesLoader = require('./fake-load/businesses');
+const InternshipsLoader = require('./fake-load/internships');
+const StudentsLoader = require('./fake-load/students');
 
 Promise.resolve().then(async () => {
     try {
         if (program.businesses) await BusinessesLoader(program.quantity, program.verbose);
+        if (program.internships) await InternshipsLoader(program.quantity, program.verbose);
+        if (program.students) await StudentsLoader(program.quantity, program.verbose);
 
         console.info(chalk.blue('Successfully setup data.'));
         process.exit(0);
