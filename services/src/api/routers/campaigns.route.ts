f3c2a455a52ac8af016682a0b2b785129dd24b26
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-10-16 15:55:51 +0200

    [API] Add missing pagination validator

--- services/src/api/routers/campaigns.route.ts
+++ services/src/api/routers/campaigns.route.ts
@@ -4,11 +4,11 @@ import { checkSchema } from 'express-validator';
 import * as CampaignsCtrl from '../controllers/campaigns.ctrl';
 
 import { ID } from '../validators/generic.val';
-import { CampaignUpdate, CampaignCreate} from '../validators/campaigns.val';
+import { CampaignUpdate, CampaignCreate, CampaignList } from '../validators/campaigns.val';
 
 const router = express.Router();
 
-router.get('', CampaignsCtrl.getCampaigns);
+router.get('', checkSchema(CampaignList), CampaignsCtrl.getCampaigns);
 router.post('', checkSchema(CampaignCreate), CampaignsCtrl.postCampaign);
 router.get('/:id', checkSchema(ID), CampaignsCtrl.getCampaign);
 router.put('/:id', checkSchema(Object.assign({}, ID, CampaignUpdate)), CampaignsCtrl.putCampaign);
