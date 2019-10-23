ade030d15aed8b066f9bbc7163d6dc88f13e1a98
Adrien Tissier <adrien.tissier@hotmail.com>
2019-10-23 12:03:08 +0200

    [ORM] Link_Internship_Files
    ça marche

--- services/src/configs/setup/database.ts
+++ services/src/configs/setup/database.ts
@@ -25,4 +25,8 @@ MentoringPropositions.belongsTo(Campaigns, { as: 'campaign', foreignKey: 'campai
 Students.hasMany(Internships, { as: 'internships', foreignKey: 'studentId', sourceKey: 'id' });
 Internships.belongsTo(Students, { as: 'student', foreignKey: 'studentId', targetKey: 'id' });
 
+// One Internship to many Files
+Internships.hasMany(Files, { as: 'files', foreignKey: 'internshipId', sourceKey: 'id' });
+Files.belongsTo(Internships, { as: 'internships', foreignKey: 'internshipId', targetKey: 'id' });
+
 export default database.sync({ force: process.env.ORM_DROP_DB_ON_START === 'true' });
