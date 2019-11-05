3e093f137b2aba446c0a0dad8fb1f79d620a65de
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-11-05 23:54:49 +0100

    [APP] Setup all campaigns mentors and admin view
    For mentor:
    * See campaign mentor dashboard
    * List all internships in a campaign
    * Consult propositions done for this campaign
    
    For admin:
    * See campaign mentor dashboard
    * List all internships in a campaign
    * List of all students in this campaign
    * List of all mentors in this campaign
    * Consult propositions done for this campaign
    * Update campaign settings

---
+++ app/src/views/campaigns/admin/dashboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ name: 'CampaignsAdminDashboard' })
+export default class extends Vue {}
+</script>
---
+++ app/src/views/campaigns/mentor/propositions.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ name: 'CampaignsMentorPropositions' })
+export default class extends Vue {}
+</script>
