be4e3ed1b6a17a70c2297c66edb3bd5e8fc7b60a
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-11-13 01:46:49 +0100

    [SERVICES] Add templates copy for devops

--- services/devops/scripts/init.js
+++ services/devops/scripts/init.js
@@ -15,6 +15,25 @@ function copyFileSync(source, target) {
     fs.writeFileSync(targetFile, fs.readFileSync(source));
 }
 
+function copyFolderSync(source, target) {
+    var files = [];
+    var targetFolder = path.join(target, path.basename(source));
+    if (!fs.existsSync(targetFolder)) {
+        fs.mkdirSync(targetFolder);
+    }
+    if (fs.lstatSync(source).isDirectory()) {
+        files = fs.readdirSync(source);
+        files.forEach(function(file) {
+            var curSource = path.join(source, file);
+            if (fs.lstatSync(curSource).isDirectory()) {
+                copyFolderSync(curSource, targetFolder);
+            } else {
+                copyFileSync(curSource, targetFolder);
+            }
+        });
+    }
+}
+
 function createIfNotExist(target) {
     if (!fs.existsSync(target)) {
         fs.mkdirSync(target);
@@ -26,6 +45,14 @@ try {
     src = path.resolve(__dirname, '../../.env');
     dest = path.resolve(__dirname, '../../dist');
     copyFileSync(src, dest);
+
+    // Copy mailer file
+    src = path.resolve(__dirname, '../../src/emails/templates');
+    dest = path.resolve(__dirname, '../../dist/emails');
+    copyFolderSync(src, dest);
+    src = path.resolve(__dirname, '../../src/emails/build');
+    dest = path.resolve(__dirname, '../../dist/emails');
+    copyFolderSync(src, dest);
 
     createIfNotExist(path.resolve(__dirname, '../../dist/caches'));
 
