b50d335a2953a8de45f8aebeb0a5f3ec5762ad26
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-11-14 20:33:51 +0100

    [WSS] Setup websocket (wss = websocket serveur)
    Done: 
    * Add socket.io module to services
    * Add express socket.io-session module to services (save socket session)
    * Add socket.io-client module to app
    * Add vue-socket.io-extended module to app (handle socket.io in vuejs)
    * Change app settings
    * Add socket init in webapp
    * Add socket init in services
    * Sync express and socket.io session to keep in memory user socket
    * Add socket connection handler to log every connection
    * Add channel to handle progress through multiple event (generic)
    * Add tests for websocket
    
    Missing: 
    * Test handler

--- services/src/server.ts
+++ services/src/server.ts
@@ -2,13 +2,19 @@ import { Server } from 'http';
 
 import logger from './utils/logger';
 
-import app from './app';
+import app, { session } from './app';
 import init from './configs/setup/init';
 
 // Require database connection initialization
 import './configs/instances/database';
 
-import setupDB from './configs/setup/database'; // Only import to setup
+import setupDB from './configs/setup/database';
+
+// Websocket server
+import socketIO from 'socket.io';
+import wss from './websocket/server';
+import * as socketHandler from './websocket/handler/handler';
+import sharedsession from 'express-socket.io-session';
 
 /**
  * Start Express server.
@@ -27,6 +33,10 @@ setupDB
             );
             console.info('  Press CTRL-C to stop\n');
         });
+        const io = socketIO(server);
+        io.use(sharedsession(session, { autoSave: true }));
+        io.on('connection', socketHandler.connectionHandler);
+        wss.setIO(io);
     })
     .catch(logger.error);
 
