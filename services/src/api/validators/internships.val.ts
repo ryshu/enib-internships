ad9bf46b72357b2729f8291052cedb2556b27ffc
Oscar Marie--Taillefer <o5mariet@enib.fr>
2019-11-15 12:08:22 +0100

    [SONAR] Reduce code duplication

--- services/src/api/validators/internships.val.ts
+++ services/src/api/validators/internships.val.ts
@@ -1,17 +1,17 @@
 import { Schema } from 'express-validator';
 
-import { paginateValidator, replaceAllExistByOptional } from './generic.val';
+import {
+    paginateValidator,
+    replaceAllExistByOptional,
+    contriesValidator,
+    addressValidator,
+} from './generic.val';
 
 const modes = ['published', 'propositions', 'self'];
 
 export const InternshipsList: Schema = {
     ...paginateValidator,
-    countries: {
-        in: ['query'],
-        isArray: { errorMessage: 'Country filter list must be array' },
-        optional: true,
-        toArray: true,
-    },
+    ...contriesValidator,
     types: {
         in: ['query'],
         isArray: { errorMessage: 'Category filter list must be array' },
@@ -71,41 +71,7 @@ export const InternshipCreate: Schema = {
         toInt: true,
     },
 
-    country: {
-        in: ['body'],
-        isString: { errorMessage: 'Country must be of type string' },
-        exists: { errorMessage: 'Country must be defined' },
-        trim: true,
-        escape: true,
-    },
-    city: {
-        in: ['body'],
-        isString: { errorMessage: 'City must be of type string' },
-        exists: { errorMessage: 'City must be defined' },
-        trim: true,
-        escape: true,
-    },
-    postalCode: {
-        in: ['body'],
-        isString: { errorMessage: 'Postal code must be of type string' },
-        exists: { errorMessage: 'Postal code must be defined' },
-        trim: true,
-        escape: true,
-    },
-    address: {
-        in: ['body'],
-        isString: { errorMessage: 'Address must be of type string' },
-        exists: { errorMessage: 'Address must be defined' },
-        trim: true,
-        escape: true,
-    },
-    additional: {
-        in: ['body'],
-        isString: { errorMessage: 'Additional must be of type string' },
-        optional: true,
-        trim: true,
-        escape: true,
-    },
+    ...addressValidator,
 
     isInternshipAbroad: {
         in: ['body'],
